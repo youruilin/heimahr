aefe8439020bd5b29d86fb4ace852472debedb66
youruilin <youruilin@gmail.com>
2025-05-14 13:49:14 +0800

    fix: test workflows 0514-1349

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,8 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  // publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/heimahr/' : '/', // 替换为你的子路径
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
